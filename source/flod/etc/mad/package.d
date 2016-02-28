be66c999d44ac8738ab8591040c9b0423c4dd587
Adrian Matoga <epi@atari8.info>
2016-02-28 12:03:53 +0100

    Many bug fixes.
    - Use package mad version 1.0.1 (older one worked by accident).
    - Catch exceptions in output.
    - Peek at larger chunk if current buffer is insufficient to play anything.
    - Use flod.traits.check.

--- source/flod/etc/mad/package.d
+++ source/flod/etc/mad/package.d
@@ -1,40 +1,74 @@
+///
 module flod.etc.mad;
 
-import flod.traits;
+import flod.pipeline;
+import flod.traits : check;
+
+///
+auto decodeMp3(Pipeline)(auto ref Pipeline pipeline)
+{
+	return pipeline.pipe!MadDecoder;
+}
+
+private:
 
 struct MadDecoder(Source, Sink) {
 	import deimos.mad;
+	static assert(mad_decoder.sizeof == 88);
 
 	Source source;
 	Sink sink;
 	const(void)* bufferStart = null;
+	size_t chunkSize = 4096;
+
 	Throwable exception;
 
-	private extern(C)
-	static mad_flow input(void* data, mad_stream* stream)
+	private mad_flow input(mad_stream* stream)
 	{
-		auto self = cast(MadDecoder*) data;
+		import core.exception : OutOfMemoryError, InvalidMemoryOperationError;
 		try {
-			if (self.bufferStart !is null) {
-				size_t cons = stream.this_frame - self.bufferStart;
-				if (cons == 0)
-					return mad_flow.STOP;
-				self.source.consume(cons);
+			size_t cons;
+			if (bufferStart !is null) {
+				cons = stream.this_frame - bufferStart;
+				if (cons > 0) {
+					source.consume(cons);
+					if (chunkSize > 4096)
+						chunkSize = 4096;
+				}
+				switch (stream.error) with (mad_error) {
+				case BUFLEN:
+					if (cons == 0)
+						chunkSize *= 2;
+					break;
+				case BUFPTR:
+					throw new InvalidMemoryOperationError("MAD error: invalid buffer pointer");
+				case NOMEM:
+					throw new OutOfMemoryError("MAD error: out of memory");
+				default:
+					break;
+				}
 			}
-			auto buf = self.source.peek(4096);
-			if (!buf.length)
+			auto buf = source.peek(chunkSize);
+			if (cons == 0 && buf.length < chunkSize)
 				return mad_flow.STOP;
-			self.bufferStart = buf.ptr;
+			bufferStart = buf.ptr;
 			mad_stream_buffer(stream, buf.ptr, buf.length);
 			return mad_flow.CONTINUE;
 		}
 		catch (Throwable e) {
-			self.exception = e;
+			exception = e;
 			return mad_flow.BREAK;
 		}
 	}
 
-	private static int scale(mad_fixed_t sample)
+	private extern(C)
+	static mad_flow inputCallback(void* data, mad_stream* stream)
+	{
+		auto self = cast(MadDecoder*) data;
+		return self.input(stream);
+	}
+
+	private static int scale(mad_fixed_t sample) @nogc nothrow pure
 	{
 		/* round */
 		sample += (1L << (MAD_F_FRACBITS - 16));
@@ -49,63 +83,72 @@ struct MadDecoder(Source, Sink) {
 		return sample >> (MAD_F_FRACBITS + 1 - 16);
 	}
 
-	private extern(C)
-	static mad_flow output(void* data, const(mad_header)* header, mad_pcm* pcm)
+	mad_flow output(const(mad_header)* header, mad_pcm* pcm)
 	{
-		auto self = cast(MadDecoder*) data;
-		uint nchannels, nsamples;
-		const(mad_fixed_t)* left_ch;
-		const(mad_fixed_t)* right_ch;
+		try {
+			uint nchannels, nsamples;
+			const(mad_fixed_t)* left_ch;
+			const(mad_fixed_t)* right_ch;
 
-		nchannels = pcm.channels;
-		nsamples  = pcm.length;
-		left_ch   = pcm.samples[0].ptr;
-		right_ch  = pcm.samples[1].ptr;
+			nchannels = pcm.channels;
+			nsamples  = pcm.length;
+			left_ch   = pcm.samples[0].ptr;
+			right_ch  = pcm.samples[1].ptr;
 
-		if (nsamples > 0) {
-			size_t size = short.sizeof * nchannels * nsamples;
-			auto buf = new ubyte[size];
-			//auto buf = self.sink.alloc(size);
-			size_t i = 0;
-			while (nsamples--) {
-				int sample = scale(*left_ch++);
-				buf[i++] = (sample >> 0) & 0xff;
-				buf[i++] = (sample >> 8) & 0xff;
-				if (nchannels == 2) {
-					sample = scale(*right_ch++);
+			if (nsamples > 0) {
+				size_t size = short.sizeof * nchannels * nsamples;
+				auto buf = new ubyte[size];
+				//auto buf = sink.alloc(size);
+				size_t i = 0;
+				while (nsamples--) {
+					int sample = scale(*left_ch++);
 					buf[i++] = (sample >> 0) & 0xff;
 					buf[i++] = (sample >> 8) & 0xff;
+					if (nchannels == 2) {
+						sample = scale(*right_ch++);
+						buf[i++] = (sample >> 0) & 0xff;
+						buf[i++] = (sample >> 8) & 0xff;
+					}
 				}
+				sink.push(buf);
+				//sink.commit(size);
 			}
-			self.sink.push(buf);
-			//self.sink.commit(size);
+		} catch (Throwable e) {
+			exception = e;
+			return mad_flow.BREAK;
 		}
 		return mad_flow.CONTINUE;
 	}
 
 	private extern(C)
-	static mad_flow error(void* data, mad_stream* stream, mad_frame* frame)
+	static mad_flow outputCallback(void* data, const(mad_header)* header, mad_pcm* pcm)
+	{
+		auto self = cast(MadDecoder*) data;
+		return self.output(header, pcm);
+	}
+
+	private extern(C)
+	static mad_flow errorCallback(void* data, mad_stream* stream, mad_frame* frame) @nogc nothrow
 	{
 		return mad_flow.CONTINUE;
 	}
 
 	void run()
 	{
-		// TODO: why is this reference 0ed after mad_decoder_init?
-		auto md = &this;
 		mad_decoder decoder;
-		mad_decoder_init(&decoder, cast(void*) md,
-			&input, null /* header */, null /* filter */,
-			&output, &error, null /* message */);
+		mad_decoder_init(&decoder, cast(void*) &this,
+			&inputCallback, null /* header */, null /* filter */,
+			&outputCallback, &errorCallback, null /* message */);
+		assert(&this !is null);
 
 		/* start decoding */
 		auto result = mad_decoder_run(&decoder, mad_decoder_mode.SYNC);
 
 		/* release the decoder */
 		mad_decoder_finish(&decoder);
-		if (md.exception)
-			throw md.exception;
+		if (exception)
+			throw exception;
 	}
 }
-static assert(isPeekSink!MadDecoder);
-static assert(isPushSource!MadDecoder);
+
+enum ch = check!("peek", "push", MadDecoder);
